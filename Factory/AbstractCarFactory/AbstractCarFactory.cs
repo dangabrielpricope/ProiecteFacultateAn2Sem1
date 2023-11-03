3f8e997de9d8ec93c89ee18735b872cfa46eb79c
dangabrielpricope <dan.pricope88@yahoo.com>
2023-11-03 21:49:56 +0200

    update

---
+++ Factory/AbstractCarFactory/AbstractCarFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Factory;
+
+namespace AbstractCarFactory
+{
+    abstract class AbstractCarFactory
+    {
+        public abstract Automobile GetAutomobile();
+        public abstract Truck GetTruck();
+
+    }
+
+    class MercedesCarFactory : AbstractCarFactory
+    {
+        public override Automobile GetAutomobile()
+        {
+            return new Automobile();
+        }
+
+        public override Truck GetTruck()
+        {
+            return new Truck();
+        }
+    }
+
+    class VolvoCarFactory : AbstractCarFactory
+    {
+        public override Automobile GetAutomobile()
+        {
+            return new Automobile();
+        }
+
+        public override Truck GetTruck()
+        {
+            return new Truck();
+        }
+    }
+}
